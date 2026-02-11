146a38fa7b8797cb81b7d30a91e5c5693d7c7f9d
Jirakit Jarusiripipat <jirakit.works@gmail.com>
2026-02-11 21:46:46 +0000

    Get Everything setup

---
+++ MovieWeb/MovieWeb/Controllers/WebHomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieWeb.Models;
+
+namespace MovieWeb.Controllers
+{
+    public class WebHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Movie movie1 = new Movie();
+            movie1.id = 1;
+            movie1.title = "Mission Impossible";
+            movie1.description = "This is a good Movie";
+            movie1.imgUrl = "http://test.com";
+            movie1.rating = 8.5f;
+
+            Movie movie2 = new Movie();
+            movie2.id = 1;
+            movie2.title = "Mission Impossible2";
+            movie2.description = "This is a greate Movie";
+            movie2.imgUrl = "http://test.com";
+            movie2.rating = 10;
+
+            List<Movie> movies = new List<Movie>();
+            movies.Add(movie1);
+            movies.Add(movie2);
+            return View(movies);
+        }
+    }
+}
---
+++ MovieWeb/MovieWeb/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieWeb.Models
+{
+    public class Movie
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string imgUrl { get; set; }
+        public float rating { get; set; }
+    }
+}
