a8e77b5917ccc0ccc058155e6dbca9c1a744bc05
Jirakit Jarusiripipat <jirakit.works@gmail.com>
2026-02-12 15:29:34 +0000

    Show table from the database

--- MovieWeb/MovieWeb/Controllers/WebHomeController.cs
+++ MovieWeb/MovieWeb/Controllers/WebHomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MovieWeb.Data;
 using MovieWeb.Models;
+using System.Net;
 
 namespace MovieWeb.Controllers
 {
@@ -13,23 +14,7 @@ namespace MovieWeb.Controllers
         }
         public IActionResult Index()
         {
-            Movie movie1 = new Movie();
-            movie1.id = 1;
-            movie1.title = "Mission Impossible";
-            movie1.description = "This is a good Movie";
-            movie1.imgUrl = "http://test.com";
-            movie1.rating = 8.5f;
-
-            Movie movie2 = new Movie();
-            movie2.id = 1;
-            movie2.title = "Mission Impossible2";
-            movie2.description = "This is a greate Movie";
-            movie2.imgUrl = "http://test.com";
-            movie2.rating = 10;
-
-            List<Movie> movies = new List<Movie>();
-            movies.Add(movie1);
-            movies.Add(movie2);
+            IEnumerable<Movie> movies = _db.Movies;
             return View(movies);
         }
 
